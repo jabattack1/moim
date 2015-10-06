2b555277ec6fce8d8906ebb7980d6d3c8e17b17a
Junho Rhee <juno0628@gmail.com>
2015-10-06 18:20:09 -0400

    added

--- app/assets/javascripts/welcomepage.js
+++ app/assets/javascripts/welcomepage.js
@@ -1,25 +1,11 @@
 $(document).ready(function() {
-	var aboutClick = false; 
-	var communityCLick = false;
 	//setting dropdown for about 
 	$('#dropdownMenu1').on('click', function() {
-		if (aboutClick == false) {
 		$(this).parent().addClass('open');
-			aboutClick = true; 
-		} else {
-			$(this).parent().removeClass('open');
-			aboutClick = false; 
-		}
 	})
 	//setting dropdown for community
 	$('#dropdownMenu2').on('click', function() {
-		if (communityCLick == false) {
-		$(this).parent().addClass('open');
-			communityCLick = true; 
-		} else {
-			$(this).parent().removeClass('open');
-			communityCLick = false; 
-		}
+		$(this).parent().addClass('open'); 
 	})
 });
 
