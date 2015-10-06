31fecc3694e1b265f90f51a34824815839d808e9
Junho Rhee <juno0628@gmail.com>
2015-10-06 18:16:19 -0400

    added dropdown

--- app/assets/javascripts/welcomepage.js
+++ app/assets/javascripts/welcomepage.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
 	var aboutClick = false; 
+	var communityCLick = false;
 	//setting dropdown for about 
-	$('.dropdown-toggle').on('click', function() {
+	$('#dropdownMenu1').on('click', function() {
 		if (aboutClick == false) {
 		$(this).parent().addClass('open');
 			aboutClick = true; 
 		} else {
 			$(this).parent().removeClass('open');
 			aboutClick = false; 
+		}
+	})
+	//setting dropdown for community
+	$('#dropdownMenu2').on('click', function() {
+		if (communityCLick == false) {
+		$(this).parent().addClass('open');
+			communityCLick = true; 
+		} else {
+			$(this).parent().removeClass('open');
+			communityCLick = false; 
 		}
 	})
 });
