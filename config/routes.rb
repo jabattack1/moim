3dd8f9d7b2f6ad5a0ac4337b0cd9eb05630862a5
Junho Rhee <juno0628@gmail.com>
2015-10-12 17:45:48 -0400

    route changed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
+  resources :blog_posts, :blogs, :events, :forums, 
+      :galleries, :presses, :replies, :teams, :users
+  get '/mission' => 'welcome#mission'
+  get '/chronicle' => 'welcome#chronicle'
+  get '/team' => 'welcome#team'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
